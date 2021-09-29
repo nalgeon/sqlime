97e4d1537f3c07c36a754117516644fd04edf046
Anton <m@antonz.org>
2021-09-29 21:35:33 +0300

    impr: auto-width for database name field

--- js/playground.js
+++ js/playground.js
@@ -94,7 +94,7 @@ async function start(name, path) {
     database.query = database.query || storage.get(database.name);
 
     document.title = database.name;
-    ui.name.value = database.name;
+    ui.name.innerText = database.name;
     ui.status.info(messages.invite);
     ui.editor.value = database.query;
     ui.editor.focus();
@@ -152,13 +152,13 @@ async function save() {
 
 // changeName changes database name
 function changeName(name) {
-    if (name == ui.name.value && name == database.name) {
+    if (name == ui.name.innerText && name == database.name) {
         return;
     }
     storage.remove(database.name);
     database.name = name;
     storage.set(name, database.query);
-    ui.name.value = name;
+    ui.name.innerText = name;
 }
 
 // showStarted shows the result of successful database load
@@ -246,8 +246,12 @@ function showDatabase(database) {
 }
 
 // User changed database name
-ui.name.addEventListener("change", (event) => {
-    changeName(event.target.value);
+ui.name.addEventListener("keydown", (event) => {
+    if (event.key != "Enter") {
+        return;
+    }
+    event.preventDefault();
+    changeName(event.target.innerText);
     event.target.blur();
 });
 
