d186b73d28d4d992e0f7259bcd67cfd56767cdf3
Anton <m@antonz.org>
2022-01-15 19:25:03 +0300

    refactor: cloud api docs

--- js/cloud.js
+++ js/cloud.js
@@ -1,3 +1,7 @@
+// Cloud Storage API Facade
+// Chooses Github Gist API for users with credentials,
+// otherwise falls back to Deta Base API.
+
 import deta from "./cloud/deta.js";
 import github from "./cloud/github.js";
 
@@ -6,22 +10,30 @@ const PROVIDERS = {
     [github.prefix]: github,
 };
 
+// Most methods delegate to the underlying provider, except for get()
 class Gister {
     constructor() {
         this._provider = null;
     }
 
+    // username is needed to choose
+    // create vs update on save
     get username() {
         return this.provider.username;
     }
 
+    // provider is the underlying cloud storage provider
+    // who does the actual work of saving and loading gists
     get provider() {
+        // lazy init the provider
         if (!this._provider) {
             this.reload();
         }
         return this._provider;
     }
 
+    // reload() chooses Github if the credentials are available
+    // otherwise chooses Deta
     reload() {
         github.loadCredentials();
         if (github.hasCredentials()) {
@@ -43,6 +55,8 @@ class Gister {
         return this.provider.getUrl(id);
     }
 
+    // get() uses the provider specified in the path value
+    // e.g. gist:12345 or deta:54321
     get(pathValue) {
         const [prefix, id] = pathValue.split(":");
         const provider = PROVIDERS[prefix];
