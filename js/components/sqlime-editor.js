bf364fda0b5e9812e1c14732edf2925a26f22874
Anton <m@antonz.org>
2023-07-06 18:01:28 +0500

    impr: convert tab to spaces (#18)

--- js/components/sqlime-editor.js
+++ js/components/sqlime-editor.js
@@ -1,4 +1,6 @@
 // SQL editor component
+const TAB_WIDTH = 2;
+
 class SqlimeEditor extends HTMLElement {
     connectedCallback() {
         if (!this.rendered) {
@@ -13,37 +15,16 @@ class SqlimeEditor extends HTMLElement {
     }
 
     listen() {
-        // Ctrl+Enter or Cmd+Enter shortcut
-        this.addEventListener("keydown", (event) => {
-            if (!event.ctrlKey && !event.metaKey) {
-                return true;
-            }
-            if (event.keyCode == 10 || event.keyCode == 13) {
-                this.dispatchEvent(
-                    new CustomEvent("execute", { detail: this.query })
-                );
-                return false;
-            }
-            return true;
-        });
-
+        // shortcuts
+        this.addEventListener("keydown", this.onKeydown.bind(this));
+        // always paste as plain text
+        this.addEventListener("paste", this.onPaste.bind(this));
         // first input event
         const onInput = (event) => {
             this.dispatchEvent(new Event("start"));
             this.removeEventListener("input", onInput);
         };
         this.addEventListener("input", onInput);
-
-        // always paste as plain text
-        this.addEventListener("paste", function (event) {
-            event.preventDefault();
-            // get text representation of clipboard
-            const text = (event.originalEvent || event).clipboardData.getData(
-                "text/plain"
-            );
-            // insert text manually
-            document.execCommand("insertHTML", false, text);
-        });
     }
 
     // focus sets cursor at the end of the editor
@@ -58,6 +39,21 @@ class SqlimeEditor extends HTMLElement {
         this.value = "";
     }
 
+    onKeydown(event) {
+        if (handleIndent(this, event)) return;
+        if (handleExecute(this, event)) return;
+    }
+
+    onPaste(event) {
+        event.preventDefault();
+        // get text representation of clipboard
+        const text = (event.originalEvent || event).clipboardData.getData(
+            "text/plain"
+        );
+        // insert text manually
+        document.execCommand("insertHTML", false, text);
+    }
+
     get value() {
         return this.innerText;
     }
@@ -69,6 +65,32 @@ class SqlimeEditor extends HTMLElement {
         const selectedQuery = window.getSelection().toString().trim();
         return selectedQuery || this.innerText;
     }
+}
+
+// handleIndent indents text with Tab
+function handleIndent(elem, event) {
+    if (event.key != "Tab") {
+        return false;
+    }
+    event.preventDefault();
+    document.execCommand("insertHTML", false, " ".repeat(TAB_WIDTH));
+    return true;
+}
+
+// handleExecute truggers 'execute' event by Ctrl/Cmd+Enter
+function handleExecute(elem, event) {
+    // Ctrl+Enter or Cmd+Enter
+    if (!event.ctrlKey && !event.metaKey) {
+        return false;
+    }
+    // 10 and 13 are Enter codes
+    if (event.keyCode != 10 && event.keyCode != 13) {
+        return false;
+    }
+
+    event.preventDefault();
+    elem.dispatchEvent(new CustomEvent("execute", { detail: elem.query }));
+    return true;
 }
 
 if (!window.customElements.get("sqlime-editor")) {
